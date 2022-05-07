170a67883d3f700662b2fbcbb51e58d064577c6f
SumitVerma786 <102235358+SumitVerma786@users.noreply.github.com>
2022-05-07 21:44:39 +0530

    Update App.js

--- TODOAPP/Js/App.js
+++ TODOAPP/Js/App.js
@@ -9,29 +9,29 @@ const addTask = () => {
     taskList.push(task);
     localStorage.setItem("Monday", JSON.stringify(taskList));
   } else if (day === "Tuesday" || day === "tuesday") {
-    tasksForTuesday = JSON.parse(localStorage.getItem("Tuesday")) || [];
-    tasksForTuesday.push(task);
-    window.localStorage.setItem("Tuesday", JSON.stringify(tasksForTuesday));
+    taskList = JSON.parse(localStorage.getItem("Tuesday")) || [];
+    taskList.push(task);
+    window.localStorage.setItem("Tuesday", JSON.stringify(taskList));
   } else if (day === "Wednesday" || day === "wednesday") {
-    tasksForWednesday = JSON.parse(localStorage.getItem("Wednesday")) || [];
-    tasksForWednesday.push(task);
-    window.localStorage.setItem("Wednesday", JSON.stringify(tasksForWednesday));
+    taskList = JSON.parse(localStorage.getItem("Wednesday")) || [];
+    taskList.push(task);
+    window.localStorage.setItem("Wednesday", JSON.stringify(taskList));
   } else if (day === "Thursday" || day === "thursday") {
-    tasksForThursday = JSON.parse(localStorage.getItem("Thursday")) || [];
-    tasksForThursday.push(task);
-    window.localStorage.setItem("Thursday", JSON.stringify(tasksForThursday));
+    taskList = JSON.parse(localStorage.getItem("Thursday")) || [];
+    taskList.push(task);
+    window.localStorage.setItem("Thursday", JSON.stringify(taskList));
   } else if (day === "Friday" || day === "friday") {
-    tasksForFriday = JSON.parse(localStorage.getItem("Friday")) || [];
-    tasksForFriday.push(task);
-    window.localStorage.setItem("Friday", JSON.stringify(tasksForFriday));
+    taskList = JSON.parse(localStorage.getItem("Friday")) || [];
+    taskList.push(task);
+    window.localStorage.setItem("Friday", JSON.stringify(taskList));
   } else if (day === "Saturday" || day === "saturday") {
-    tasksForSaturday = JSON.parse(localStorage.getItem("Saturday")) || [];
-    tasksForSaturday.push(task);
-    window.localStorage.setItem("Saturday", JSON.stringify(tasksForSaturday));
+    taskList = JSON.parse(localStorage.getItem("Saturday")) || [];
+    taskList.push(task);
+    window.localStorage.setItem("Saturday", JSON.stringify(taskList));
   } else if (day === "Sunday" || day === "sunday") {
-    tasksForSunday = JSON.parse(localStorage.getItem("Sunday")) || [];
-    tasksForSunday.push(task);
-    window.localStorage.setItem("Sunday", JSON.stringify(tasksForSunday));
+    taskList = JSON.parse(localStorage.getItem("Sunday")) || [];
+    taskList.push(task);
+    window.localStorage.setItem("Sunday", JSON.stringify(taskList));
   } else {
     alert("Please enter a valid day");
   }
